b5b325c9f3393822109045adb7c8335c80f9c38d
Cherie Huang <cheriemhuang@gmail.com>
2018-07-19 18:15:38 -0700

    Removing unnecessary files

--- entrypoint.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-printenv | sed 's/^\(.*\)$/export \1/g' > /app/project_env.sh && touch /var/log/cron.log && cron && tail -f /var/log/cron.log & python3 /app/myhackerrank/manage.py runserver 0.0.0.0:8000 & python3 -u /app/myhackerrank/hrank-consumer.py
